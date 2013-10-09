065df7faa8737af6b7676e6bec1a7e6f560b9f86
Mariusz Wojtach <mariusz.wojtach@gmail.com>
2013-10-09 20:59:08 +0200

    biblioteka RoboSpice dla zadań asynchronicznych

--- src/pl/us/wiinom/cameraman/MainActivity.java
+++ src/pl/us/wiinom/cameraman/MainActivity.java
@@ -1,22 +1,69 @@
 package pl.us.wiinom.cameraman;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
 
 public class MainActivity extends Activity {
 
+	private SpiceManager spiceManager = new SpiceManager(SimpleService.class);
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button startBtn = (Button) findViewById(R.id.button1);
+		
+		startBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				
+				UploadRequest request = new UploadRequest();
+				UploadRequestListener requestListener = new UploadRequestListener();
+				
+				spiceManager.execute(request, requestListener);
+			}
+			
+		});
+		
+	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		spiceManager.start(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onStop() {
+		spiceManager.shouldStop();
+		super.onStop();
+	}
+	
+	class UploadRequestListener implements RequestListener<String> {
+
+		@Override
+		public void onRequestFailure(SpiceException arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onRequestSuccess(String arg0) {
+			TextView tv = (TextView) findViewById(R.id.hello);
+			
+			tv.setText(arg0);
+		}
+		
 	}
 
 }
