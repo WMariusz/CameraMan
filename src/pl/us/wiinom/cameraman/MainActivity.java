821b375e9f41d303e695d4cbed80742d282d8068
Mariusz Wojtach <mariusz.wojtach@gmail.com>
2013-10-12 14:22:29 +0200

    Strzelanie zdjęć bez wychodzenia do aplikaji aparatu

--- src/pl/us/wiinom/cameraman/MainActivity.java
+++ src/pl/us/wiinom/cameraman/MainActivity.java
@@ -1,10 +1,25 @@
 package pl.us.wiinom.cameraman;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.octo.android.robospice.SpiceManager;
@@ -14,29 +29,55 @@ import com.octo.android.robospice.request.listener.RequestListener;
 
 public class MainActivity extends Activity {
 
-	private SpiceManager spiceManager = new SpiceManager(SpiceService.class);
+	private SpiceManager spiceManager = new SpiceManager(SimpleService.class);
+	private Uri uri;
+	private int PICTURE = 0;
+	private Bitmap photo;
+	private Camera camera;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		camera = Camera.open();
+		
 		Button startBtn = (Button) findViewById(R.id.button1);
 		
 		startBtn.setOnClickListener(new OnClickListener() {
-
 			@Override
 			public void onClick(View v) {
+				
+				try {
+					//set camera parameters
+					//camera.setParameters(parameters);
+					camera.startPreview();
+					camera.takePicture(null, null, mCall);
+				} catch (RuntimeException e) {
+					Log.d("CAMERA", e.getMessage());
+				}
 				
 				//SpiceRequest request = new SpiceRequest();
 				//UploadRequestListener requestListener = new UploadRequestListener();
 				
 				//spiceManager.execute(request, requestListener);
 			}
-			
 		});
 		
 	}
+	
+	Camera.PictureCallback mCall = new Camera.PictureCallback()
+    {
+
+		@Override
+		public void onPictureTaken(byte[] image, Camera camera) {
+			Bitmap photo = BitmapFactory.decodeByteArray(image, 0, image.length);
+			ImageView iv = (ImageView) findViewById(R.id.imageView1);
+			iv.setImageBitmap(photo);
+		}
+  
+      
+    };
 	
 	@Override
 	protected void onStart() {
@@ -60,9 +101,7 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onRequestSuccess(String arg0) {
-			TextView tv = (TextView) findViewById(R.id.hello);
 			
-			tv.setText(arg0);
 		}
 		
 	}
